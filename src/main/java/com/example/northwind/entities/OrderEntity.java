6eb02f2fb21ed6533d99c9e9128740e4c945a218
InduLekha98 <rindulekha09@gmail.com>
2026-07-22 19:34:41 -0400

    enity relationship

---
+++ src/main/java/com/example/northwind/entities/OrderEntity.java
@@ -0,0 +1,73 @@
+package com.example.northwind.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private int orderId;
+
+
+    @Column(name = "customer_id")
+    private String customerId;
+
+    @Column(name = "employee_id")
+    private int employee;
+
+    @Column(name = "order_date")
+    private LocalDate orderDate;
+
+    @Column(name = "required_date")
+    private LocalDate requiredDate;
+
+    @Column(name = "shipped_date")
+    private LocalDate shippedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "ship_via")
+    private int shipper;
+
+    @Column(name = "freight")
+    private float freight;
+
+    @Column(name = "ship_name")
+    private String shipName;
+
+    @Column(name = "ship_address")
+    private String shipAddress;
+
+    @Column(name = "ship_city")
+    private String shipCity;
+
+    @Column(name = "ship_region")
+    private String shipRegion;
+
+    @Column(name = "ship_postal_code")
+    private String shipPostalCode;
+
+    @Column(name = "ship_country")
+    private String shipCountry;
+
+    @OneToMany(mappedBy = "orders")
+    private List<OrderDetail> orderDetails;
+    // Getters and setters omitted for brevity
+}
+
+
+
